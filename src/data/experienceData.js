49082d35f68d1d8ecf4fcf43e47fd9d0ce44b74a
Aman Gondaliya <35319382+Amangondaliya555@users.noreply.github.com>
2023-03-06 11:48:36 +0530

    Update experienceData.js

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -9,19 +9,26 @@ export const experienceData = [
     {
         id: 2,
         company: 'Nokia | Chennai',
+        jobtitle: 'Associate',
+        startYear: 'January 2023 ',
+        endYear: ' Present'
+    },
+    {
+        id: 3,
+        company: 'Nokia | Chennai',
         jobtitle: 'Graduate Engineer Trainee',
         startYear: 'July 2021 ',
         endYear: ' Present'
     },
     {
-        id: 3,
+        id: 4,
         company: 'Property Advisor & Architecture | Remote',
         jobtitle: 'Team Leader Intern',
         startYear: 'April 2021 ',
         endYear: ' July 2021'
     },
     {
-        id: 4,
+        id: 5,
         company: 'JP Morgan Virtual Experience | Remote',
         jobtitle: 'Software Engineering Intern',
         startYear: 'April 2020 ',
