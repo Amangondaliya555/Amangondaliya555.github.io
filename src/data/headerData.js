fb2cc1d87a8ed718b45bf142ac131c4c4b77fa2b
amang <amangondaliya555@gmail.com>
2023-12-31 13:13:47 +0000

    updating repo

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,7 +4,7 @@ import img from '../assets/png/profileimg.png'
 export const headerData = {
     name: 'Aman Gondaliya',
     title: "Software Developer",
-    desciption:"I still use Stackoverflow over ChatGPT. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
+    desciption:"You create a gun module, a gun class, a foot module and a foot class. After realising you can't point the gun at the foot, you pass a reference to the gun to a foot object. After the foot is blown up, the gun object remains alive for eternity, ready to shoot all future feet that may happen to appear. [stolen from Mark] ",
     image: img,
     resumePdf: resume
 }
