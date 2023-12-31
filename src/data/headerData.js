f544ed99d1984c97a4d865ba7ec312292c828c19
Aman Gondaliya <35319382+Amangondaliya555@users.noreply.github.com>
2023-12-31 10:38:28 +0000

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,7 +4,7 @@ import img from '../assets/png/profileimg.png'
 export const headerData = {
     name: 'Aman',
     title: "Software Engineer",
-    desciption: "    I still use Stackoverflow over ChatGPT.",
+    desciption: "I still use Stackoverflow over ChatGPT.",
     image: img,
     resumePdf: resume
 }
