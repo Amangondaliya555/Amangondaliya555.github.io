c3a2c6fa17fec415d0e8ee70fe21f2979ddf0eaf
amang <amangondaliya555@gmail.com>
2023-12-31 13:01:51 +0000

    updating repo

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,7 +4,7 @@ import img from '../assets/png/profileimg.png'
 export const headerData = {
     name: 'Aman Gondaliya',
     title: "Software Developer",
-    desciption:"I still use Stackoverflow over ChatGPT. ",
+    desciption:"I still use Stackoverflow over ChatGPT. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
     image: img,
     resumePdf: resume
 }
